65acfad303c9cda7a620251932dc35fe7ea63ec6
LLlaaaiiimmm <denivash005@mail.ru>
2025-05-24 14:10:04 +0300

    Initial Commit

---
+++ networks/networksApp.swift
@@ -0,0 +1,17 @@
+//
+//  networksApp.swift
+//  networks
+//
+//  Created by Denis Ivaschenko on 24.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct networksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ networksTests/networksTests.swift
@@ -0,0 +1,17 @@
+//
+//  networksTests.swift
+//  networksTests
+//
+//  Created by Denis Ivaschenko on 24.05.2025.
+//
+
+import Testing
+@testable import networks
+
+struct networksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
