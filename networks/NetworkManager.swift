1630584cabc409959d8ca0283e0562f74b69c92e
metelykx <ivaschenkoodenys@gmail.com>
2025-05-25 21:43:38 +0300

    decode

--- networks/NetworkManager.swift
+++ networks/NetworkManager.swift
@@ -19,9 +19,20 @@ final class NetworkManager {
     func getNews() async throws -> News{
         
         guard let url = URL(string: URLNews) else {throw NetworkError.invalidURL}
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { throw NetworkError.invalidResponse}
+        
+        do {
+            return try JSONDecoder().decode(News.self, from: data)
+        }
+        catch {
+            throw NetworkError.InvalidData
+        }
     }
 }
 
 enum NetworkError: String, Error {
     case invalidURL = "Invalid URL"
+    case invalidResponse = "Invalid response"
+    case InvalidData = "Invalid Data"
 }
